38443e9aae16e881cd20ffb2c36e22847a97bbce
ASalazar46 <andysal737@gmail.com>
2021-09-19 13:54:59 -0700

    Added files

---
+++ Review1.js
@@ -0,0 +1,9 @@
+// This is a single line comment
+/* This is a multi line 
+    comment */
+
+var variable; // This is a variable, declare it with var and give its name
+variable = 5; // "=" is the assignment operator, set variable to 5
+
+var elbairav;
+elbairav = variable;
